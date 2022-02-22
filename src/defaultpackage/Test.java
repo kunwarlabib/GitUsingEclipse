e54f075ef1f067b11aef5c2415c3a82be46f11c6
Labib A <kahmed@cloudlex.com>
2022-02-22 17:59:59 +0530

    Committing first time

---
+++ src/defaultpackage/Test.java
@@ -0,0 +1,13 @@
+package defaultpackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		
+		if(hungry=="yes")
+			System.out.println("eat something");
+		else
+			System.out.println("Dnt eat ");
+	}
+}
